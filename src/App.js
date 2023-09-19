f6909b337ed90f0b8e3ae7136152cf505602a643
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2023-09-20 05:03:16 +0700

    ah it's not working

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   const [result, setResult] = useState(0);//this is a calculator app, it is bound to have one variable to keep result to do with future operations
   const [numberValue, setNumberValue] = useState("");
 
+  const opsSym = ['+', '-', '*', '/', ','];
+
   function plus(){
     //result = result + Number(inputRef.current.value); okay i try this out of curiosity, to know what will be the error now i know
     setResult((result) => result + Number(inputRef.current.value));
@@ -36,12 +38,11 @@ function App() {
     inputRef.current.value=0;
   }
 
-    console.log("Result: "+ result);
-
-
-  /*function numberInput(){
-
-  }*/
+  const addToEquation = (tempVal) => {setNumberValue(numberValue + tempVal.target.value)
+    if(numberValue.slice(-1) === opsSym || numberValue.slice(-1) === " "){
+      numberValue.pop();
+    }
+  }
   
   return (
     <div>
@@ -49,23 +50,23 @@ function App() {
       <p>here is your input: {numberValue}</p>
       <p>Result: {result}</p>
       <input pattern="[0-9]" type="number" placeholder="your number, please" ref={inputRef}></input>
-      <button onClick={tempVal => setNumberValue(numberValue + tempVal.target.value)/*plus*/} value="+">Add</button>
-      <button onClick={tempVal => setNumberValue(numberValue + tempVal.target.value)/*minus*/} value="-">Subtract</button>
-      <button onClick={tempVal => setNumberValue(numberValue + tempVal.target.value)/*multiply*/} value="*">Multiply</button>
-      <button onClick={tempVal => setNumberValue(numberValue + tempVal.target.value)/*divide*/} value="/">Devide</button>
+      <button onClick={addToEquation/*plus*/} value="+">Add</button>
+      <button onClick={addToEquation/*minus*/} value="-">Subtract</button>
+      <button onClick={addToEquation/*multiply*/} value="*">Multiply</button>
+      <button onClick={addToEquation/*divide*/} value="/">Devide</button>
       <button onClick={resetResult}>Reset Result</button>
       <button onClick={resetInput}>Reset Input</button>
       
-      <button className="numberButton" value="1" onClick={tempVal => setNumberValue(numberValue + tempVal.target.value)}>1</button>
-      <button className="numberButton" value="2" onClick={tempVal => setNumberValue(numberValue + tempVal.target.value)}>2</button>
-      <button className="numberButton" value="3" onClick={tempVal => setNumberValue(numberValue + tempVal.target.value)}>3</button>
-      <button className="numberButton" value="4" onClick={tempVal => setNumberValue(numberValue + tempVal.target.value)}>4</button>
-      <button className="numberButton" value="5" onClick={tempVal => setNumberValue(numberValue + tempVal.target.value)}>5</button>
-      <button className="numberButton" value="6" onClick={tempVal => setNumberValue(numberValue + tempVal.target.value)}>6</button>
-      <button className="numberButton" value="7" onClick={tempVal => setNumberValue(numberValue + tempVal.target.value)}>7</button>
-      <button className="numberButton" value="8" onClick={tempVal => setNumberValue(numberValue + tempVal.target.value)}>8</button>
-      <button className="numberButton" value="9" onClick={tempVal => setNumberValue(numberValue + tempVal.target.value)}>9</button>
-      <button className="numberButton" value="0" onClick={tempVal => setNumberValue(numberValue + tempVal.target.value)}>0</button>
+      <button className="numberButton" value="1" onClick={addToEquation}>1</button>
+      <button className="numberButton" value="3" onClick={addToEquation}>3</button>
+      <button className="numberButton" value="2" onClick={addToEquation}>2</button>
+      <button className="numberButton" value="4" onClick={addToEquation}>4</button>
+      <button className="numberButton" value="5" onClick={addToEquation}>5</button>
+      <button className="numberButton" value="6" onClick={addToEquation}>6</button>
+      <button className="numberButton" value="7" onClick={addToEquation}>7</button>
+      <button className="numberButton" value="8" onClick={addToEquation}>8</button>
+      <button className="numberButton" value="9" onClick={addToEquation}>9</button>
+      <button className="numberButton" value="0" onClick={addToEquation}>0</button>
       <button className="resultButton" value="=" onClick={tempVal => setNumberValue(eval(numberValue))}>=</button>
     </div>
   );
